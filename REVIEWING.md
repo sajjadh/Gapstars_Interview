3c5eb54f48d8f239f7a8107ac147ff330bc810a4
Philip Mander <philip@harver.com>
2019-11-03 21:57:34 +0100

    fix: wrong @harver/bat require path in setup.js

---
+++ REVIEWING.md
@@ -0,0 +1,7 @@
+How to review the completed exercise
+====================================
+
+1. Can the candidate clone and set up a basic Git/Node.JS repository?
+2. Can the candidate find the bugs in the API? There are 2 critical bugs, 2 major and 2 minor (search `server.js` for `BUG:` to find them)
+3. How does the candidate automate the tests? Bonus for taking the hint and using Bat.
+4. Did the canddiate understand the point about implementing failing tests to demonstrate a bug?
--- features/support/setup.js
+++ features/support/setup.js
@@ -1,12 +1,6 @@
 const { setWorldConstructor, After, AfterAll, Before, BeforeAll, Given, When, Then } = require('cucumber');
-const { registerHooks, World: BaseWorld, registerSteps } = require('../../server');
+const { registerHooks, World: BatWorld, registerSteps } = require('@harver/bat');
 
-class World extends BaseWorld {
-    constructor() {
-        super();
-    }
-}
-
-setWorldConstructor(World);
+setWorldConstructor(BatWorld);
 registerHooks({ After, AfterAll, Before, BeforeAll });
 registerSteps({ Given, When, Then });
